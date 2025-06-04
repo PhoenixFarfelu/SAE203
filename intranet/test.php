cee637f3dfd40e636d7858649731685b3b639b4a
user <user@mrte210093.iutsm.etu.univ-rennes1.fr>
2025-06-04 16:45:14 +0200

    changement test et début style

--- intranet/test.php
+++ intranet/test.php
@@ -2,15 +2,19 @@
 include "scripts/fonctions.php";
 parametre('img/logo1.png');
 // annuaire_client();
-echo '<body>';
-if (isset($_GET['submit'])){
-    if (explode('/',$_GET['submit'])[1] == 'gestionnaire_fichier'){
-        affiche_dossier($_GET['submit'],'./gestionnaire_fichier/groupe');
-    } else {
-        affiche_dossier('./gestionnaire_fichier/groupe/test1/test2/test3','./gestionnaire_fichier/groupe');
-    }
-} else {
-    affiche_dossier('./gestionnaire_fichier/groupe/test1/test2/test3','./gestionnaire_fichier/groupe');
-}
-echo '</body>';
+
+// Gestionnaire de fichier
+// echo '<body>';
+// if (isset($_GET['submit'])){
+//     if (explode('/',$_GET['submit'])[1] == 'gestionnaire_fichier'){
+//         affiche_dossier($_GET['submit'],'./gestionnaire_fichier/groupe');
+//     } else {
+//         affiche_dossier('./gestionnaire_fichier/groupe/test1/test2/test3','./gestionnaire_fichier/groupe');
+//     }
+// } else {
+//     affiche_dossier('./gestionnaire_fichier/groupe/test1/test2/test3','./gestionnaire_fichier/groupe');
+// }
+// echo '</body>';
+
+annuaire_client();
 ?>
