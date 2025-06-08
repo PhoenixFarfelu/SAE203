9946a49363963c89358cff77abcfb262440f78e9
télio <roussel.telio@gmail.com>
2025-06-08 15:36:37 +0200

    amélioration navbar

---
+++ intranet/annuaire_fournisseur_partenaire.php
@@ -0,0 +1,12 @@
+<?php
+session_start();
+include 'scripts/fonctions.php';
+parametre('img/icon1.png');
+
+if (!isset($_SESSION['nom'])) {
+    header('Location: connexion.php');
+    exit();
+}
+entete();
+navigation();
+?>
