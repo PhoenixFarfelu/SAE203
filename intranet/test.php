c7af6168c505bdb8ee15e2b68272b55fb1af5768
Neven Louër <neven.louer@etudiant.univ-rennes.fr>
2025-06-12 23:42:52 +0200

    Ajout de personne dans l'entreprise

--- intranet/test.php
+++ intranet/test.php
@@ -1,6 +1,6 @@
 <?php
 // session_start();
-// include "scripts/fonctions.php";
+include "scripts/fonctions.php";
 // print_r($_SESSION);
 // annuaire_client();
 
@@ -17,7 +17,24 @@
 // }
 // echo '</body>';
 
-$test = array(1 => "hbuhbu", 2 => "ihbiubh");
-file_put_contents('data/annuaires/entreprise.json', json_encode($test, JSON_PRETTY_PRINT));
+// $test = array(1 => "hbuhbu", 2 => "ihbiubh");
+// file_put_contents('data/annuaires/entreprise.json', json_encode($test, JSON_PRETTY_PRINT));
+$data = read("data/annuaires/entreprise.json", $JSON=true);
+$file = read("data/utilisateur/utilisateurs.json", $JSON=true);
+foreach ($data as $value) {
+    $temp = array(
+        "nom"=>$value['nom'],
+        "prenom"=> $value['prenom'],
+        "utilisateur"=>strtolower(substr($value['prenom'], 0, 1) . $value['nom']),
+        "motdepasse"=> '$2y$10$TAl6KgD8D/8zDmvcOvezx.ED.RUhgpNnG5Ae46V6XbqoTNc/dSINm',
+        "role"=>$value['role'],
+        "email"=>strtolower($value['prenom'] . '.' . $value['nom']) . '@domaine.fr'
+    );
+    $file[]=$temp;
+}
+echo '<pre>';
+print_r($file);
+echo '</pre>';
+file_put_contents("data/utilisateur/utilisateurs.json", json_encode($file, JSON_PRETTY_PRINT));
 
 ?>
