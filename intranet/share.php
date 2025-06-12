5a1dd61460c84b9fa0116f2dbb3dc4dd54016bbe
Meven_BOONE <meven.boone@etudiant.univ-rennes.fr>
2025-06-12 06:19:58 +0200

    déportation de fonctions et partage de fichier 1/2

--- intranet/share.php
+++ intranet/share.php
@@ -2,64 +2,80 @@
 session_start();
 include 'scripts/fonctions.php';
 parametre('img/logo1.png');
+if (isset($_POST["filename"])) {
+        echo '
+        <body class="container-fluid row"
+            <div class="container-fluid col-sm-5 bg-white shadow rounded-end p-5" align-items: left; justify-content: center;">
+            <div class="w-100">
+            <div class="container-fluid">
+            <a><img src="img/logo2.png" alt="Logo" class="img-fluid" style="max-width: 150px;"></a>
+                <h1 class="text-center" >Partager un fichier</h1>                                   
+                <form action="" method="post" class="">
+                    <!-- Nom d\'utilisateur -->
+                    <div class="mb-3">
+                        <label for="utilisateur" class="form-label">Partager à </label>
+                        <input type="text" class="form-control" id="utilisateur" name="utilisateur" placeholder="Nom d\'utilisateur" required>
+                    </div>
+                    <input type="text" class="form-control" id="utilisateur" name="utilisateur" placeholder="Nom d\'utilisateur" required>
+                    <!-- Role -->
+                    <div class="mb-3">
+                        <label for="role" class="form-label">Permissions</label>
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" name="perm[]" id="view" value="view" checked>
+                            <label class="form-check-label" for="view">Lecture</label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" name="perm[]" id="edit" value="edit">
+                            <label class="form-check-label" for="edit">Ecriture</label>
+                        </div>
+                        <div class="form-check">
+                            <input class="form-check-input" type="checkbox" name="perm[]" id="delete" value="delete">
+                            <label class="form-check-label" for="delete">Suppression</label>
+                        </div>
+                    </div>
 
-echo '
-    <body class="container-fluid row">
+                    <!-- Submit -->
+                    <button type="submit" class="btn btn-dark" name="submit" value="submit">Partager</button>
+                </form> 
+                <div class="row pt-3">
+                    <a href="gestionnaire_fichier.php" class="btn btn-dark" class="col">Annuler</a>
+                </div>       
+            </div>
+        </body>';
+} else {
+    header('Location: gestionnaire_fichier.php');
+    exit();
+}
 
-        <div class="container-fluid col-sm-5 bg-white shadow rounded-end p-5" align-items: left; justify-content: center;">
-        <div class="w-100">
-        <div class="container-fluid">
-        <a><img src="img/logo2.png" alt="Logo" class="img-fluid" style="max-width: 150px;"></a>
-            <h1 class="text-center" >Partager un fichier</h1>                                   
-            <form action="ajout_employe.php" method="post" class="">
-
-                <!-- Nom d\'utilisateur -->
-                <div class="mb-3">
-                    <label for="utilisateur" class="form-label">Nom d\'utilisateur</label>
-                    <input type="text" class="form-control" id="utilisateur" name="utilisateur" placeholder="Nom d\'utilisateur" required>';
-                
-                if (isset($_SESSION['erreur']) && $_SESSION['erreur'] === 'Nom d\'utilisateur déjà utilisé') {
-                    echo '<div class="alert alert-danger mt-1" role="alert">Nom d\'utilisateur déjà utilisé</div>';
-                    unset($_SESSION['erreur']);
+if (isset($_POST["submit"])) {
+    if (isset($_POST["filename"]) && is_owner($_POST["filename"])) {
+        if (is_file($_POST["filename"])) {
+            $user = $_SESSION["nom"] ?? "";
+            $permissions = get_file_permissions($_POST["filename"]);
+            if (!in_array($_POST["utilisateur"],$permissions["shared_with"])) {
+                array_push($permissions["shared_with"], $_POST["utilisateur"]);
+            }
+            if (!in_array($_POST["utilisateur"],$permissions["can_view"])) {
+                if (in_array("view",$_POST["perm"])) {
+                    array_push($permissions["can_view"], $_POST["utilisateur"]);
                 }
-                echo '
-                </div>
-
-                <!-- Role -->
-                <div class="mb-3">
-                    <label for="role" class="form-label">Permissions</label>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="perm[]" id="role_admin" value="view" checked>
-                        <label class="form-check-label" for="role_admin">Lecture</label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="perm[]" id="role_salaries" value="edit">
-                        <label class="form-check-label" for="role_salaries">Ecriture</label>
-                    </div>
-                    <div class="form-check">
-                        <input class="form-check-input" type="checkbox" name="perm[]" id="role_managers" value="delete">
-                        <label class="form-check-label" for="role_managers">Suppression</label>
-                    </div>
-                </div>
-
-                <!-- Submit -->
-                <button type="submit" class="btn btn-dark" name="submit" value="submit">S\'inscrire</button>
-            </form> 
-            <div class="row pt-3">
-                <a href="annuaire_entreprise.php" class="btn btn-dark" class="col">Annuler</a>
-            </div>       
-        </div>
-    </body>';
-
-if (isset($_POST["filename"]) && is_owner($_POST["filename"])) {
-    if (is_file($_POST["filename"])) {
-        $user = $_SESSION["nom"] ?? "";
-        $permissions = get_file_permissions($_POST["filename"]);
-        array_push($permissions["shared_with"], $_POST["filename"]);
-        echo '<meta http-equiv="refresh" content="0;url=gestionnaire_fichier.php">';
-    } else {
-        echo "<script>alert('un probleme est survenu');</script>";
-        echo '<meta http-equiv="refresh" content="0;url=gestionnaire_fichier.php">';
+            }
+            if (!in_array($_POST["utilisateur"],$permissions["can_edit"])) {
+                if (in_array("edit",$_POST["perm"])) {
+                    array_push($permissions["can_edit"], $_POST["utilisateur"]);
+                }
+            }
+            if (!in_array($_POST["utilisateur"],$permissions["can_delete"])) {
+                if (in_array("delete",$_POST["perm"])) {
+                    array_push($permissions["can_delete"], $_POST["utilisateur"]);
+                }
+            }
+            echo "<script>alert('fichier partagé avec succès');</script>";
+            echo '<meta http-equiv="refresh" content="0;url=gestionnaire_fichier.php">';
+        } else {
+            echo "<script>alert('un probleme est survenu');</script>";
+            echo '<meta http-equiv="refresh" content="0;url=gestionnaire_fichier.php">';
+        }
     }
 }
 ?>
