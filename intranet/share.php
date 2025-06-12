5419cc3c1f4c1da3f78571ccda36d77a9c9100d2
Meven_BOONE <meven.boone@etudiant.univ-rennes.fr>
2025-06-12 07:50:41 +0200

    partage de fichiers 2/2

--- intranet/share.php
+++ intranet/share.php
@@ -2,6 +2,12 @@
 session_start();
 include 'scripts/fonctions.php';
 parametre('img/logo1.png');
+
+if (!isset($_SESSION['nom'])) {
+    header('Location: connexion.php');
+    exit();
+}
+
 if (isset($_POST["filename"])) {
         echo '
         <body class="container-fluid row"
@@ -16,7 +22,6 @@ if (isset($_POST["filename"])) {
                         <label for="utilisateur" class="form-label">Partager à </label>
                         <input type="text" class="form-control" id="utilisateur" name="utilisateur" placeholder="Nom d\'utilisateur" required>
                     </div>
-                    <input type="text" class="form-control" id="utilisateur" name="utilisateur" placeholder="Nom d\'utilisateur" required>
                     <!-- Role -->
                     <div class="mb-3">
                         <label for="role" class="form-label">Permissions</label>
@@ -26,13 +31,15 @@ if (isset($_POST["filename"])) {
                         </div>
                         <div class="form-check">
                             <input class="form-check-input" type="checkbox" name="perm[]" id="edit" value="edit">
-                            <label class="form-check-label" for="edit">Ecriture</label>
+                            <label class="form-check-label" for="edit">Ecriture (et Lecture)</label>
                         </div>
                         <div class="form-check">
                             <input class="form-check-input" type="checkbox" name="perm[]" id="delete" value="delete">
-                            <label class="form-check-label" for="delete">Suppression</label>
+                            <label class="form-check-label" for="delete">Suppression (et lecture et ecriture)</label>
                         </div>
                     </div>
+                    <input type="text" name="filename" value="'.$_POST["filename"].'" requiered hidden>
+
 
                     <!-- Submit -->
                     <button type="submit" class="btn btn-dark" name="submit" value="submit">Partager</button>
@@ -50,26 +57,33 @@ if (isset($_POST["filename"])) {
 if (isset($_POST["submit"])) {
     if (isset($_POST["filename"]) && is_owner($_POST["filename"])) {
         if (is_file($_POST["filename"])) {
-            $user = $_SESSION["nom"] ?? "";
             $permissions = get_file_permissions($_POST["filename"]);
+            if (!isset($_POST["perm"])) {
+                echo("<script>alert('Veuillez selectionner des permissions à accorder');</script>");
+                exit;
+            }
             if (!in_array($_POST["utilisateur"],$permissions["shared_with"])) {
                 array_push($permissions["shared_with"], $_POST["utilisateur"]);
             }
-            if (!in_array($_POST["utilisateur"],$permissions["can_view"])) {
-                if (in_array("view",$_POST["perm"])) {
+            if (!in_array($_POST["utilisateur"],$permissions["can_view"]) && in_array("view",$_POST["perm"])) {
+                array_push($permissions["can_view"], $_POST["utilisateur"]);
+            }
+            if (!in_array($_POST["utilisateur"],$permissions["can_edit"]) && in_array("edit",$_POST["perm"])) {
+                if (!in_array($_POST["utilisateur"],$permissions["can_view"])) {
                     array_push($permissions["can_view"], $_POST["utilisateur"]);
                 }
+                array_push($permissions["can_edit"], $_POST["utilisateur"]);
             }
-            if (!in_array($_POST["utilisateur"],$permissions["can_edit"])) {
-                if (in_array("edit",$_POST["perm"])) {
+            if (!in_array($_POST["utilisateur"],$permissions["can_delete"]) && in_array("delete",$_POST["perm"])) {
+                if (!in_array($_POST["utilisateur"],$permissions["can_delete"])) {
+                    if (!in_array($_POST["utilisateur"],$permissions["can_view"])) {
+                        array_push($permissions["can_view"], $_POST["utilisateur"]);
+                    }
                     array_push($permissions["can_edit"], $_POST["utilisateur"]);
                 }
+                array_push($permissions["can_delete"], $_POST["utilisateur"]);
             }
-            if (!in_array($_POST["utilisateur"],$permissions["can_delete"])) {
-                if (in_array("delete",$_POST["perm"])) {
-                    array_push($permissions["can_delete"], $_POST["utilisateur"]);
-                }
-            }
+            file_put_contents($_POST["filename"].".meta.json", json_encode($permissions, JSON_PRETTY_PRINT));
             echo "<script>alert('fichier partagé avec succès');</script>";
             echo '<meta http-equiv="refresh" content="0;url=gestionnaire_fichier.php">';
         } else {
